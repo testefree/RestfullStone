eab08efa94f7ce4e9840eee4ddef5822dee56d26
5653ccee2d5271416d0001a9 <vagtonaf@hotmail.com>
2017-07-06 00:28:20 -0300

    Add project files.

---
+++ RestfullStone/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RestfullStone.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ RestfullStone/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RestfullStone.WebApiApplication" Language="C#" %>
